3a45a9dc0761fd4ab0c3e8617450f68a44f5dae6
tythompson <tythompson@gmail.com>
2016-07-13 00:41:56 -0400

    james is a troll
    -    expect(s.last.latitude).to eq -36
    +   expect(s.last.longitude).to eq -36

--- locatable_specs.rb
+++ locatable_specs.rb
@@ -1,7 +1,38 @@
 require "rspec"
+require "haversine"
+require "pry"
+
 
 module Locatable
+  def Locatable.included station
+    station.extend Locatable::ClassMethods
+  end
+
+  module ClassMethods
+    def closest_to(lat, long, opts={})
+      station_vector = {}
+      self.all.each do |station|
+        station_vector[station] = (Haversine.distance(station.latitude, station.longitude, lat, long).to_miles)
+      end
+      station_vector =  station_vector.sort_by{ |k,v| v}
+      # turns into array
+      if opts[:count] && opts[:count] > 1
+        stations = []
+        station_vector[0...opts[:count]].each {|v| stations << v[0]}
+        stations
+      else
+        station_vector[0][0]
+      end
+    end
+  end
+
+  def distance_to(lat, long)
+    distance = Haversine.distance(self.latitude, self.longitude, lat, long)
+    distance.to_miles
+  end
 end
+
+
 
 class Station
   attr_reader :latitude, :longitude
@@ -36,10 +67,10 @@ describe Locatable do
     expect(s.longitude).to eq -33
   end
 
-  skip "can find list of closest" do
+  it "can find list of closest" do
     s = Station.closest_to 10, -34, count: 2
 
     expect(s.count).to eq 2
-    expect(s.last.latitude).to eq -36
+    expect(s.last.longitude).to eq -36
   end
 end
